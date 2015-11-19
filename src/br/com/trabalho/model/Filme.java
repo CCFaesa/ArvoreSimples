02a69219acc849b1e913ead096654683e0b30372
wiliamtrancoso <wileleh@gmail.com>
2015-11-19 00:47:17 -0200

    [develop] [wiliamtrancoso]   \_> adicionado atributos ao filme

--- src/br/com/trabalho/model/Filme.java
+++ src/br/com/trabalho/model/Filme.java
@@ -3,6 +3,8 @@ public class Filme {
 	
 	private int id;
 	private String nome;
+	private Genero genero;
+	private int anoLancamento;
 
 	public Filme(int chave) {
 		this.id = chave;
@@ -14,6 +16,30 @@ public class Filme {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
 	}
 	
 }
