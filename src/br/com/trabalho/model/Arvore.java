c204b9aac8f57f1584ace0f31b68d49e0011f394
wiliamtrancoso <wileleh@gmail.com>
2015-11-20 05:22:00 -0200

    [develop] [WILIAM TRANCOSO]   \_> Arvore compara elementos pelo nome e em ordem alfabetica

--- src/br/com/trabalho/model/Arvore.java
+++ src/br/com/trabalho/model/Arvore.java
@@ -35,7 +35,7 @@ public class Arvore {
 			NoArv novo = new NoArv(elem);
 			return novo;
 		}else {
-			if (elem.getId() < no.getInfo().getId()){
+			if (elem.getNome().compareTo(no.getInfo().getNome()) < 0){
 				no.setEsq(inserir(elem, no.getEsq()));
 				return no;
 			}else{
