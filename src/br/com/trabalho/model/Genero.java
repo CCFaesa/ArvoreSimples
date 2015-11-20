67bbcfd728c94901ce4c8e77a9246b01b28fa70d
wiliamtrancoso <wileleh@gmail.com>
2015-11-20 04:06:22 -0200

    [develop] [WILIAM TRANCOSO]   \_> gerado toString   \_> fix descricoes

--- src/br/com/trabalho/model/Genero.java
+++ src/br/com/trabalho/model/Genero.java
@@ -2,7 +2,7 @@ package br.com.trabalho.model;
 
 public enum Genero {
 	
-	ACAO(1, "Ação"), AVENTURA(2, "Aventura"), COMEDIA(3, "Comédia"), DRAMA(4, "DRAMA"), TERROR(5, "TERROR");
+	ACAO(1, "Ação"), AVENTURA(2, "Aventura"), COMEDIA(3, "Comédia"), DRAMA(4, "Drama"), TERROR(5, "Terror");
 	
 	private int id;
 	private String descricao;
@@ -26,5 +26,10 @@ public enum Genero {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getDescricao();
 	}
 }
