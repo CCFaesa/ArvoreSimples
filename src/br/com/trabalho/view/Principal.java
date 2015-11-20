ef403f5ca8b61f4673b59a845ba24076d4e1a843
wiliamtrancoso <wileleh@gmail.com>
2015-11-20 04:08:22 -0200

    [develop] [WILIAM TRANCOSO]   \_> Adicionado titulo principal   \_> adicionado campos de cadastro de filme(sem funcionalidade)

--- src/br/com/trabalho/view/Principal.java
+++ src/br/com/trabalho/view/Principal.java
@@ -3,16 +3,24 @@ package br.com.trabalho.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Frame;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
 import br.com.trabalho.global.Design;
+import br.com.trabalho.model.Genero;
 import br.com.trabalho.view.componente.CFolha;
 import br.com.trabalho.view.componente.CLinha;
 
@@ -22,6 +30,19 @@ public class Principal extends JFrame{
 	
 	private JPanel frame;
 	private JScrollPane scroll;
+	
+	private JLabel titulo;
+	
+	private JLabel lblNome;
+	private JLabel lblGenero;
+	private JLabel lblAno;
+	private JLabel lblTituloCadastro;
+	
+	private JTextField txtNome;
+	private JComboBox<Genero> cbxGeneros;
+	private JTextField txtAno;
+	
+	private JButton btnCadastrar;
 	
 	private ArrayList<CFolha> folhas;
 	
@@ -35,18 +56,11 @@ public class Principal extends JFrame{
 		
 		principal.proximasExecucoes();
 		principal.repaint();
-		
 	}
 	
 	
 	private void proximasExecucoes() {
-			}
-	public Principal() {
-		frame = new JPanel();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setVisible(true);
 	}
-
 	
 	public Principal(String title) {
 		super(title);
@@ -58,6 +72,7 @@ public class Principal extends JFrame{
 			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 			this.setSize(1410, 960);
 			this.setLayout(new BorderLayout());
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -72,6 +87,61 @@ public class Principal extends JFrame{
 		frame.setBackground(Design.BACKGROUND_PRINCIPAL);
         frame.setPreferredSize(new Dimension(1380, 900));
         frame.setLayout(null);
+        
+        titulo = new JLabel("Árvore Binária de Filmes");
+        lblNome = new JLabel("Nome:", SwingConstants.RIGHT);
+        lblGenero = new JLabel("Genero:", SwingConstants.RIGHT);
+        lblAno = new JLabel("Ano:", SwingConstants.RIGHT);
+        lblTituloCadastro = new JLabel("Cadastro Filmes");
+        
+        txtNome = new JTextField();
+        cbxGeneros = new JComboBox<Genero>(Genero.values());
+        txtAno = new JTextField();
+        
+        btnCadastrar = new JButton("Cadastrar");
+        
+        titulo.setBounds(525, 50, 350, 35);
+        lblNome.setBounds(45, 788, 55, 20);
+        lblGenero.setBounds(45, 821, 55, 20);
+        lblAno.setBounds(305, 821, 55, 20);
+        lblTituloCadastro.setBounds(45, 750, 200, 25);
+        
+        txtNome.setBounds(110, 785, 335, 30);
+        cbxGeneros.setBounds(110, 818, 200, 30);
+        txtAno.setBounds(365, 818, 80, 30);
+        
+        btnCadastrar.setBounds(325, 860, 120, 30);
+        
+        
+        lblNome.setFont(Design.FONTE_PADRAO);
+        lblGenero.setFont(Design.FONTE_PADRAO);
+        lblAno.setFont(Design.FONTE_PADRAO);
+        txtNome.setFont(Design.FONTE_PADRAO);
+        cbxGeneros.setFont(Design.FONTE_PADRAO);
+        txtAno.setFont(Design.FONTE_PADRAO);
+        btnCadastrar.setFont(Design.FONTE_PADRAO);
+        lblTituloCadastro.setFont(Design.FONTE_TITULO2);
+        titulo.setFont(Design.FONTE_TITULO1);
+        
+        titulo.setForeground(Design.TITULO_PRINCIPAL);
+        
+        JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
+        separator.setBounds(40, 775, 410, 5);
+        frame.add(separator);
+        
+        frame.add(titulo);
+
+        frame.add(lblNome);
+        frame.add(lblGenero);
+        frame.add(lblAno);
+        frame.add(lblTituloCadastro);
+        
+        frame.add(txtNome);
+        frame.add(cbxGeneros);
+        frame.add(txtAno);
+        
+        frame.add(btnCadastrar);
+        
 	}
 	
 	private void adicionaElementos() {
