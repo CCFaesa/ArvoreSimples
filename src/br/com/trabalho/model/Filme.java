714789cfac8446d2953febb922760e0d7f2bb8bb
wiliamtrancoso <wileleh@gmail.com>
2015-11-19 23:15:48 -0200

    [develop] [WILIAM TRANCOSO]   \_> id do filme eh auto-incrementado   \_> id do filme so acessivel para set   \_> add construtores do Filme

--- src/br/com/trabalho/model/Filme.java
+++ src/br/com/trabalho/model/Filme.java
@@ -1,21 +1,34 @@
 package br.com.trabalho.model;
 public class Filme {
 	
-	private int id;
+	private static int contado = 0;
+	
+	private int id ;
 	private String nome;
 	private Genero genero;
 	private int anoLancamento;
 
-	public Filme(int chave) {
-		this.id = chave;
+	public Filme() {
+		super();
+		this.id = ++contado;
+	}
+	
+	public Filme(String nome) {
+		this();
+		this.nome = nome;
+	}
+	
+	
+	
+	public Filme(String nome, Genero genero, int anoLancamento) {
+		this();
+		this.nome = nome;
+		this.genero = genero;
+		this.anoLancamento = anoLancamento;
 	}
 
 	public int getId() {
 		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public String getNome() {
